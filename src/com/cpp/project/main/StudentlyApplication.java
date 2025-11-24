f402c0f6f9609d41e9a96fabb16e3f31571156ec
talhaahmed884 <talhaahmed884@gmail.com>
2025-11-23 22:13:22 -0800

    1. added sample application.properties 2. added db driver 3. added spring boot application driver

---
+++ src/com/cpp/project/main/StudentlyApplication.java
@@ -0,0 +1,11 @@
+package com.cpp.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentlyApplication.class, args);
+    }
+}
