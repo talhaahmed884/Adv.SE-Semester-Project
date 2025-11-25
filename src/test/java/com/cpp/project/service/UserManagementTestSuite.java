b602300d1a5da5d15cec8a2650f49ed6816ee584
talhaahmed884 <talhaahmed884@gmail.com>
2025-11-25 05:43:08 -0800

    1. Attached set and verify password use case test cases with concerned test driver

--- src/test/java/com/cpp/project/service/UserManagementTestSuite.java
+++ src/test/java/com/cpp/project/service/UserManagementTestSuite.java
@@ -9,8 +9,10 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SuiteDisplayName("Studently - Complete Test Suite")
 @SelectPackages({
+        "com.cpp.project.uc_1_set_password",
         "com.cpp.project.uc_1_signup",
         "com.cpp.project.uc_2_login",
+        "com.cpp.project.uc_2_verify_password"
 })
 @SelectClasses({
         AuthControllerIntegrationTest.class
